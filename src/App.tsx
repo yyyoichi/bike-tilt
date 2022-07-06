c9277b0f1314f2879b722639f86f890c5f41f1b5
yyyoichi <origamiichiro@gmail.com>
2022-07-07 07:49:37 +0900

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import Top from './pages/index';
+import Play from './pages/Play'
+import { Route, Routes } from 'solid-app-router';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>hello</p>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="" component={Top} />
+        <Route path="" component={Play} />
+      </Routes>
+    </>
   );
 };
 
