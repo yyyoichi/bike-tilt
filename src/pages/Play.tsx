66067dd602f27df10ae4ab1549c65b9c191f3281
yyyoichi <origamiichiro@gmail.com>
2022-07-07 18:45:55 +0900

    傾き検知

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,5 +1,30 @@
+import { createSignal } from "solid-js"
+
 export default function Play() {
+    const [isEnable, setEnable] = createSignal(false)
+    const [motion, setMotion] = createSignal("no")
+    // DeviceOrientationEvent.prototype.
+    const iosPermission = async () => {
+        if (typeof( DeviceMotionEvent ) !== "undefined" && typeof (DeviceOrientationEvent as any).requestPermission === 'function') {
+            const res = await (DeviceOrientationEvent as any).requestPermission()
+            if (res !== "granted") {
+                return setMotion("You can not use.")
+            }
+        }
+        window.addEventListener("deviceorientation", event => {
+            console.log(event)
+            setMotion(JSON.stringify(event["beta"]))
+        }, true)
+        setEnable(true)
+    }
+
     return (
-        <div>play!</div>
+        <div style={{ backgroundColor: "#666" }}>
+            <div>play!</div>
+            <p>{motion()}</p>
+            {
+                isEnable()? <></>: <input type="submit" value={"start"} onclick={iosPermission}/>
+            }
+        </div>
     )
 }
